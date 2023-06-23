080a9acd7903d70e6d5ef198a223dc5dc3050ba6
Kishore <kishore.sai37@gmail.com>
2023-06-23 20:14:54 +0530

    update about us

--- components/about/index.jsx
+++ components/about/index.jsx
@@ -2,37 +2,41 @@ import Image from 'next/image';
 const About = () => {
   return (
     <section
-      className='section'
       id='about'
+      className=' bg-accent'
     >
-      <h2 className='mb-5 text-2xl font-bold text-center sm:text-3xl text-primary'>
-        Why <span className='text-accent'>Relearn School</span>
-      </h2>
-      <div className='grid gap-8 md:grid-cols-2 place-items-center'>
-        <div className='border-[3px] border-solid border-accent rounded-lg'>
-          <Image
-            src='/assets/images/hero.jpg'
-            alt='hero'
-            width={0}
-            height={0}
-            style={{ width: '100%', height: 'auto' }}
-          />
+      <div className='flex flex-col items-center justify-between section '>
+        <h2 className='mb-5 text-2xl font-bold text-center sm:text-3xl text-primary'>
+          Why <span className='text-secondary'>Relearn School</span>
+        </h2>
+
+        <p className='text-lg font-medium text-center text-primary'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          rerum magnam nostrum at provident accusamus voluptatibus maxime
+          tempora pariatur impedit!
+        </p>
+        <div className='grid gap-8 md:grid-cols-3 sm:grid-cols-1 place-items-center'>
+          <article className='shadow-lg card bg-secondary text-primary'>
+            <div className='card-body'>
+              <h2 className='card-title'>Card title!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </article>
+          <article className='shadow-lg card bg-secondary text-primary'>
+            <div className='card-body'>
+              <h2 className='card-title'>Card title!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </article>
+          <article className='shadow-lg card bg-secondary text-primary'>
+            <div className='card-body'>
+              <h2 className='card-title'>Card title!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </article>
         </div>
-        <div>
-          <div className='font-bold sm:text-[1.875rem] text-[1.5rem] mb-5 text-primary'>
-            We provide the <br /> best
-            <span className='text-accent'> learning Experience</span>
-          </div>
-          <p className='mb-4 text-sm leading-7 text-primary'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Repudiandae, totam earum. Minus deleniti repellat id! Ratione quia
-            eum, explicabo quos eos magni vel corporis voluptatibus, inventore
-            doloremque aliquam pariatur recusandae.
-          </p>
-          <button className='px-6 py-3 text-sm font-bold border border-solid rounded-lg border-primary text-primary'>
-            Know More
-          </button>
-        </div>
+
+        <button className='btn-custom btn-custom-main'>Know More</button>
       </div>
     </section>
   );
