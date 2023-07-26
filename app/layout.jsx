b4a1ae5d2d92d8fa2d1507c539a5f14b7d270b4a
Kishore <kishore.sai37@gmail.com>
2023-07-26 17:20:46 +0530

    modify analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -73,15 +73,16 @@ const RootLayout = ({ children }) => {
           strategy='afterInteractive'
           dangerouslySetInnerHTML={{
             __html: `!function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window,document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '1954325961591455'); 
-                fbq('track', 'PageView');`,
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window,document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '660667355687232'); 
+                    fbq('track', 'PageView');
+                    `,
           }}
         />
 
@@ -89,11 +90,20 @@ const RootLayout = ({ children }) => {
           <img
             height='1'
             width='1'
-            src='https://www.facebook.com/tr?id=1954325961591455&ev=PageView
+            src='https://www.facebook.com/tr?id=660667355687232&ev=PageView
 &noscript=1'
             alt='facebook'
           />
         </noscript>
+
+        <Script
+          id='microsoft_clarity'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){ c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}; t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i; y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y); })(window, document, "clarity", "script", "i4ua5pagbl");
+            `,
+          }}
+        />
       </head>
       <body>
         <ModalProvider>{children}</ModalProvider>
